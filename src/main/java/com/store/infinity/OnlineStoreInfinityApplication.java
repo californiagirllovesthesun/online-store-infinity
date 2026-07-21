386731caf2a07a6af46a222b9817a6f54567098d
Katie Carlson <katiecarlson@Mac.lan>
2026-07-21 16:56:24 -0700

    Add pom.xml and Spring Boot main class

---
+++ src/main/java/com/store/infinity/OnlineStoreInfinityApplication.java
@@ -0,0 +1,11 @@
+package com.store.infinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreInfinityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineStoreInfinityApplication.class, args);
+    }
+}
